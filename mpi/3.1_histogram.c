5f988d8b53bd17ec9facd0d5d6959f311cc14640
Gabriel S. Colombo <gscolombo404@gmail.com>
2026-01-29 23:06:45 -0300

    reimplementation

--- mpi/3.1_histogram.c
+++ mpi/3.1_histogram.c
@@ -3,75 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void get_and_distr_input(int rank, int p, int *n, int *bins, int *local_n,
-                         float *local_data, float *bin_maxes, float *min_data) {
-  float *data;
-
-  /* Read input data */
-  if (!rank) {
-    printf("N = ");
-    fflush(stdout);
-    scanf("%i", n);
-
-    if (*n <= 0) {
-      printf("Invalid input. N must be greater than or equal to 1.\n");
-      MPI_Abort(MPI_COMM_WORLD, 1);
-    }
-
-    printf("Number of bins: ");
-    fflush(stdout);
-    scanf("%i", bins);
-    if (*bins <= 0) {
-      printf("Invalid input. An histogram must have at least one bin.\n");
-      MPI_Abort(MPI_COMM_WORLD, 1);
-    }
-
-    data = (float *)malloc(*n * sizeof(float));
-    if (!data) {
-      printf("Error allocating memory for input data.");
-      MPI_Abort(MPI_COMM_WORLD, 1);
-    }
-
-    float max_data = -INFINITY;
-    *min_data = INFINITY;
-
-    printf("Enter values separated by space: \n");
-    for (int i = 0; i < *n; i++) {
-      scanf("%f", &data[i]);
-
-      // Find minimum and maximum data values
-      if (data[i] < *min_data)
-        *min_data = data[i];
-
-      if (data[i] > max_data)
-        max_data = data[i];
-    }
-
-    printf("Minimum value: %g\n", *min_data);
-    printf("Maximum value: %g\n", max_data);
-
-    // Define upper bound for every bin
-    bin_maxes = (float *)malloc(*bins * sizeof(float));
-
-    if (!bin_maxes) {
-      printf("Error during memory allocation for bins upper bounds.\n");
-      MPI_Abort(MPI_COMM_WORLD, 1);
-    }
-
-    const float bin_width = (max_data - *min_data) / *bins;
-
-    for (int i = 0; i < *bins; i++)
-      bin_maxes[i] = *min_data + bin_width * (i + 1);
-  }
-
-  // Send number of bins and bins upper bounds to every process
-  MPI_Bcast(bins, 1, MPI_COUNT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(bin_maxes, *bins, MPI_INT, 0, MPI_COMM_WORLD);
-
-  /* Distribute data over process */
-  int k, r;
-  int *displacements;
+float *get_local_data(int rank, int p, int n, int *local_n, float *data) {
+  float *local_data;
   int *sendcounts = (int *)malloc(p * sizeof(int));
+  int *displacements;
 
   if (!sendcounts) {
     printf("Error during memory allocation for local N array.\n");
@@ -79,8 +14,8 @@ void get_and_distr_input(int rank, int p, int *n, int *bins, int *local_n,
   }
 
   if (!rank) {
-    k = *n / p; // k data points for every process
-    r = *n % p; // r < p remaining data points
+    int k = n / p;
+    int r = n % p;
 
     displacements = (int *)malloc(p * sizeof(int));
 
@@ -102,6 +37,12 @@ void get_and_distr_input(int rank, int p, int *n, int *bins, int *local_n,
   // Send local N array for every process
   MPI_Bcast(sendcounts, p, MPI_INT, 0, MPI_COMM_WORLD);
   *local_n = sendcounts[rank];
+  local_data = (float *)malloc(*local_n * sizeof(float));
+
+  if (!local_data) {
+    printf("Error during memory allocation for local N array.\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
 
   // Send data slice for each process
   if (!rank) {
@@ -115,6 +56,69 @@ void get_and_distr_input(int rank, int p, int *n, int *bins, int *local_n,
                  *local_n, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
   free(sendcounts);
+
+  return local_data;
+}
+
+float *get_bin_maxes(float max_data, float min_data, int bins) {
+  float *bin_maxes = (float *)malloc(bins * sizeof(float));
+
+  if (!bin_maxes) {
+    printf("Error during memory allocation for bins upper bounds.\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  const float bin_width = (max_data - min_data) / bins;
+
+  for (int i = 0; i < bins; i++)
+    bin_maxes[i] = min_data + bin_width * (i + 1);
+
+  return bin_maxes;
+}
+
+float *get_input(int *n, int *bins, float *max_data, float *min_data) {
+  float *data;
+  *max_data = -INFINITY;
+  *min_data = INFINITY;
+
+  printf("N = ");
+  fflush(stdout);
+  scanf("%i", n);
+
+  if (*n <= 0) {
+    printf("Invalid input. N must be greater than or equal to 1.\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  printf("Number of bins: ");
+  fflush(stdout);
+  scanf("%i", bins);
+  if (*bins <= 0) {
+    printf("Invalid input. An histogram must have at least one bin.\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  data = (float *)malloc(*n * sizeof(float));
+  if (!data) {
+    printf("Error allocating memory for input data.");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  *min_data = INFINITY;
+
+  printf("Enter values separated by space: \n");
+  for (int i = 0; i < *n; i++) {
+    scanf("%f", &data[i]);
+
+    // Find minimum and maximum data values
+    if (data[i] < *min_data)
+      *min_data = data[i];
+
+    if (data[i] > *max_data)
+      *max_data = data[i];
+  }
+
+  return data;
 }
 
 void count_data(int n, int bins, int *bin_count, float *data,
@@ -138,26 +142,35 @@ void output_histogram(int bins, int *bin_count, float *bin_maxes,
 
 int main() {
   int n, bins, rank, p, local_n;
-  float *local_data, *bin_maxes, min_data;
   int *bin_count, *local_bin_count;
 
+  float *local_data, *bin_maxes, *data;
+  float max_data, min_data;
+
   local_data = bin_maxes = NULL;
+  bin_count = NULL;
 
   MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  get_and_distr_input(rank, p, &n, &bins, &local_n, local_data, bin_maxes,
-                      &min_data);
-
-  // Initialize bin count arrays
   if (!rank) {
-    bin_count = (int *)calloc(bins, sizeof(int));
+    int *bin_count = (int *)calloc(bins, sizeof(int));
+
     if (!bin_count) {
       printf("Error during memory allocation for global histogram.\n");
       MPI_Abort(MPI_COMM_WORLD, 1);
     }
+
+    data = get_input(&n, &bins, &max_data, &min_data);
   }
+
+  // TODO: Use MPI derived datatype
+  MPI_Bcast(&bins, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&max_data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&min_data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  bin_maxes = get_bin_maxes(max_data, min_data, bins);
 
   local_bin_count = (int *)calloc(bins, sizeof(int));
 
@@ -165,6 +178,8 @@ int main() {
     printf("Error during memory allocation for local histogram.\n");
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
+
+  local_data = get_local_data(rank, p, n, &local_n, data);
 
   count_data(local_n, bins, local_bin_count, local_data, bin_maxes);
 
